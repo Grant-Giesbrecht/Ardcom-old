2f85d3547198f95fab3edee041fe7ba217a7960f
Mos-Fett <giesbrechtgrant@gmail.com>
2017-07-18 10:24:51 -0400

    Add files via upload

--- Ardcom.hpp
+++ Ardcom.hpp
@@ -1,6 +1,6 @@
 //
 //  Ardcom.hpp
-//  
+//
 //
 //  Created by Grant Giesbrecht on 6/29/17.
 //
@@ -12,11 +12,24 @@
 #include <stdio.h>
 #include <string>
 
-//MacBook Pro USB Port Macros
-#define USB_LEFT "/dev/cu.usbmodem1411"
-#define USB_RIGHT "/dev/cu.usbmodem1421"
-#define USB_LEFT_ALT "/dev/cu.wchusbserial1410"
-#define USB_RIGHT_ALT "/dev/cu.wchusbserial1420"
+#ifdef _WIN32
+	#define USB_1 "COM1"
+	#define USB_2 "COM2"
+	#define USB_3 "COM3"
+	#define USB_4 "COM4"
+	#define USB_5 "COM5"
+#elif __APPLE__
+	//MacBook Pro USB Port Macros
+	#define USB_LEFT "/dev/cu.usbmodem1411"
+	#define USB_RIGHT "/dev/cu.usbmodem1421"
+	#define USB_LEFT_ALT "/dev/cu.wchusbserial1410"
+	#define USB_RIGHT_ALT "/dev/cu.wchusbserial1420"
+#elif __linux__
+	#define USB "/dev/ttyACM0"
+	#define USB_ALT "/dev/ttyUSB0"
+#else
+  #error "Unknown compiler"
+#endif
 
 //Transmission Mode Codes
 #define FN_VERIFY 0x0A
@@ -40,41 +53,45 @@
 
 class Ardcom{
 public:
-    
+
     //Opening commands
+	Ardcom();
     Ardcom(std::string port);
+	~Ardcom();
+	bool open(std::string);
+	void close();
     bool is_open();
 
     //Auxilliary opening commands
     void wait_for_MCU(double seconds);
     bool verify_connection();
-    
+
     //Setup commands
     void enablePinControl(bool b);
     bool getPinControlEnabled();
-    
+
     //Parametric Commands
     void read_MCU_parameter(char parameter, float& value);
     void set_MCU_parameter(char parameter, float value);
     void ping_float(char parameter, float send, float& recieve);
-    
+
     //Pin Commands
     void pinMode(int pin, int mode);
     void digitalWrite(int pin, int value);
     int digitalRead(int pin);
     int analogRead(int pin);
-    
+
     bool send_float(float x); //Write a float (as 4 chars) (non-blocking)
     bool recieve_float(float& x); //Read a float (as 4 chars) (blocking)
-    
+
     void send_char(char x); //Write a char (non-blocking)
     void recieve_char(char& x); //Read a char (blocking)
-    
+
 private:
-    
+
     FILE* file; //Connects to MCU
     bool is_open_bool;
-    
+
 };
 
 #endif /* Ardcom_hpp */
